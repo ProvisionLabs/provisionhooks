25e3898c451bfc933fcc5ea37f96bd342759be72
João Paulo Gomes Rodrigues <jpgomesr07@gmail.com>
2026-04-04 17:19:32 -0300

    refactor: Implement Clients struct for managing webhook channels and enhance synchronization

--- store/store.go
+++ store/store.go
@@ -11,12 +11,57 @@ type WebhooksReceived struct {
 	webhooks []request.WebhookRequest
 }
 
+type Clients struct {
+	mu       sync.Mutex
+	channels map[chan request.WebhookRequest]struct{}
+}
+
 func NewWebhooksReceived() *WebhooksReceived {
 	return &WebhooksReceived{}
+}
+
+func NewClients() *Clients {
+	return &Clients{
+		channels: make(map[chan request.WebhookRequest]struct{}),
+	}
 }
 
 func (whs *WebhooksReceived) Add(wHook request.WebhookRequest) {
 	whs.mu.Lock()
 	defer whs.mu.Unlock()
 	whs.webhooks = append(whs.webhooks, wHook)
+}
+
+func (whs *WebhooksReceived) ListAll() []request.WebhookRequest {
+	whs.mu.Lock()
+	defer whs.mu.Unlock()
+
+	webhooks := make([]request.WebhookRequest, len(whs.webhooks))
+	copy(webhooks, whs.webhooks)
+	return webhooks
+}
+
+func (clt *Clients) Add(ch chan request.WebhookRequest) {
+	clt.mu.Lock()
+	defer clt.mu.Unlock()
+	clt.channels[ch] = struct{}{}
+}
+
+func (clt *Clients) Remove(ch chan request.WebhookRequest) {
+	clt.mu.Lock()
+	defer clt.mu.Unlock()
+	delete(clt.channels, ch)
+	close(ch)
+}
+
+func (clt *Clients) Broadcast(req request.WebhookRequest) {
+	clt.mu.Lock()
+	defer clt.mu.Unlock()
+
+	for ch := range clt.channels {
+		select {
+		case ch <- req:
+		default:
+		}
+	}
 }
